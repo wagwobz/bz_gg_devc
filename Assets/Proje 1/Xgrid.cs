3407b35f6c6359dd903fbdf610d9797449b590d2
Behzat Zihni <55581179+wagwobz@users.noreply.github.com>
2025-03-01 15:59:50 +0300

    Grid Creation
    Grid Creation logic

---
+++ Assets/Proje 1/Xgrid.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Proje_1
+{
+    public class Xgrid : MonoBehaviour
+    {
+        public int size = 5;
+
+        float _startingPosValue;
+        Vector3 _startPos;
+
+        [SerializeField] XgridCell cellPrefab;
+        [SerializeField] List<XgridCell> cells = new List<XgridCell>();
+
+        [ContextMenu("Create Grid")]
+        public void ReCalculateGrid() {
+            DestroyCells();
+            _startingPosValue = size / 2f - 0.5f;
+            _startPos = new Vector3(-_startingPosValue, 0, -_startingPosValue);
+            CreateCells(size, _startPos);
+        }
+
+        void DestroyCells() {
+            for (int i = 0; i < cells.Count; i++) {
+                DestroyImmediate(cells[i].gameObject);
+            }
+            cells.Clear();
+        }
+
+        void CreateCells(int gridSize, Vector3 startingPos) {
+            for (int i = 0; i < gridSize * gridSize; i++) {
+                var x = i % 5;
+                var z = i / 5;
+                var pos = startingPos + new Vector3(x, 0, z);
+                var cell = Instantiate(cellPrefab, pos, Quaternion.identity, transform);
+                cell.gameObject.name = "Cell" + i;
+                cells.Add(cell);
+            }
+        }
+    }
+}
---
+++ Assets/Proje 1/XgridCell.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Proje_1
+{
+    public class XgridCell : MonoBehaviour
+    {
+        public bool markedX = false;
+        [SerializeField] GameObject xGameObject;
+
+        [ContextMenu("Mark X")]
+        public void MarkX() {
+            markedX = true;
+            xGameObject.SetActive(true);
+        }
+
+        [ContextMenu("Unmark X")]
+        public void UnmarkX() {
+            markedX = false;
+            xGameObject.SetActive(false);
+        }
+    }
+}
